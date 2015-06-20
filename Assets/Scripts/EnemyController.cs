e44e9d81be6d91e4e5df79bfc04d5ee3c534a459
Regner Blok-Andersen <shadowdf@gmail.com>
2015-06-20 00:48:24 +0000

    Initial commit... new to Unity and never done Unity in Git... this should be interesting.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	
+	public int health;
+
+	private GameController gameControllerScript;
+
+	void Start () {
+		GameObject gameController = GameObject.Find ("Game Controller");
+		gameControllerScript = gameController.GetComponent<GameController> ();
+		
+		UpdateHealthText ();
+	}
+	
+	void OnMouseUp () {
+		int damage = gameControllerScript.GetDamageAmmount();
+		DamageTaken (damage);
+	}
+
+	void DamageTaken (int damage) {
+		health -= damage;
+		Debug.Log ("Enemy lost " + damage + " HP. Now has " + health + " HP remaining.");
+
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
+
+		UpdateHealthText ();
+	}
+
+	void UpdateHealthText () {
+		gameObject.GetComponentInChildren<TextMesh>().text = "" + health;
+	}
+
+	void OnDestroy () {
+		Debug.Log ("Health equal to or below 0. Death.");
+		gameControllerScript.RemoveEnemy (gameObject);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+	// Public Variables
+
+	// Enemy NPC Stuff
+	public GameObject enemy;
+	public Transform[] enemySpawnPoints;
+
+	// Player Stuff
+	public GameObject player;
+	public Transform playerSpawnPoint;
+	public int baseDamage;
+
+	// UI Stuff
+	public Text istText;
+	public Text curreLevelText;
+
+	// Private Variables
+	private long isk;
+	private int currentLevel;
+	private List<GameObject> spawnedEnemies;
+	
+	void Start () {
+		spawnedEnemies = new List<GameObject> ();
+
+		// Testing shit, go away
+		isk = 100000000000000;
+
+		SpawnPlayer ();
+		IncrementLevel ();
+	}
+
+	void Update () {
+		UpdateISKText ();
+	}
+
+	void IncrementLevel () {
+		currentLevel += 1;
+
+		UpdateLevelText ();
+		SpawnEnemies ();
+	}
+
+	void SpawnPlayer () {
+		Instantiate (player, playerSpawnPoint.position, playerSpawnPoint.rotation);
+	}
+	
+	void SpawnEnemies () {
+		// We should never spawn enemies unless all are dead so confirm that first.
+		if (spawnedEnemies.Count == 0) {
+			foreach (Transform spawnPoint in enemySpawnPoints) {
+				GameObject newEnemy = Instantiate (enemy, spawnPoint.position, spawnPoint.rotation) as GameObject;
+				spawnedEnemies.Add (newEnemy);
+			}
+		}
+	}
+
+	void UpdateISKText () {
+		istText.text = "ISK:\n" + isk;
+	}
+
+	void UpdateLevelText () {
+		curreLevelText.text = "Level:\n" + currentLevel;
+	}
+
+	public int GetDamageAmmount () {
+		return baseDamage;
+	}
+
+	public void RemoveEnemy (GameObject enemyToDie) {
+		spawnedEnemies.Remove (enemyToDie);
+
+		if (spawnedEnemies.Count <= 0) {
+			IncrementLevel ();
+		}
+	}
+}
